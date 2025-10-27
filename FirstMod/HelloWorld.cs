549ef0a5819d998c47f22edecf0cdadd53fbf5b0
grassfrobber <239922425+grassfrobber@users.noreply.github.com>
2025-10-26 21:25:59 -0400

    chore: simplify hello world

--- FirstMod/HelloWorld.cs
+++ FirstMod/HelloWorld.cs
@@ -2,24 +2,21 @@
 
 namespace FirstMod
 {
+    // Runs before and after Db.Initialize() in Assembly-CSharp.dll
+    [HarmonyPatch(typeof(Db))]
+    [HarmonyPatch("Initialize")]
     public class HelloWorld
     {
-        public static string version = "0.0.3";
+        public static string version = "0.0.4";
 
-        // Runs before and after Db.Initialize() in Assembly-CSharp.dll
-        [HarmonyPatch(typeof(Db))]
-        [HarmonyPatch("Initialize")]
-        public class Db_Initialize_Patch
+        public static void Prefix()
         {
-            public static void Prefix()
-            {
-                Debug.Log($"FirstMod v{version}: Hello, world. Before Db.Initialize");
-            }
+            Debug.Log($"FirstMod v{version}: Hello, world. Before Db.Initialize");
+        }
 
-            public static void Postfix()
-            {
-                Debug.Log($"FirstMod v{version}: After Db.Initialize");
-            }
+        public static void Postfix()
+        {
+            Debug.Log($"FirstMod v{version}: After Db.Initialize");
         }
     }
 }
